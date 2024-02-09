c2e0bf8082f00b6213cf68324fab15a58fbfa72b
Vasu <vasumendapara93@gmail.com>
2024-02-09 11:25:45 +0530

    Destroying the modal
    After submitting the login or registering button the form should be destroy or close

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, OnDestroy } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 
@@ -7,7 +7,7 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit{
+export class ModalComponent implements OnInit,OnDestroy{
   @Input() modalID = ''
   constructor(
     public modal: ModalService,
@@ -18,6 +18,10 @@ export class ModalComponent implements OnInit{
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement)
+  }
+
+  ngOnDestroy() {
+      document.body.removeChild(this.el.nativeElement)
   }
 
   closeModal(){

59e3e37680113fc62e2bbed70f11276984596c63
Vasu <vasumendapara93@gmail.com>
2024-02-09 10:47:59 +0530

    Fire Base Connected

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,21 +2,27 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
+  public isAuthenticated$: Observable<boolean>
   constructor(private auth: AngularFireAuth,
     private db: AngularFirestore) { 
       this.usersCollection = db.collection('users')
+      this.isAuthenticated$ = auth.user.pipe(
+        map(user=>!!user)
+      )
     }
 
   public async createUser(userData: IUser){
-    // if(!userData.password){
-    //   throw new Error('Password not provided!');
-    // }
+    if(!userData.password){
+      throw new Error('Password not provided!');
+    }
     const userCred = await this.auth.createUserWithEmailAndPassword(
       userData.email as string, userData.password as string
    )

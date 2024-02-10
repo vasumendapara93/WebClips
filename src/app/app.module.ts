c6d61c6bec921bff8eaaed4410fd31a0edab46ba
Vasu <vasumendapara93@gmail.com>
2024-02-10 11:03:56 +0530

    Route Parameter for video clips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
+import { ClipComponent } from './clip/clip.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { VideoModule } from './video/video.module';
     AppComponent,
     NavComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ClipComponent
   ],
   imports: [
     BrowserModule,

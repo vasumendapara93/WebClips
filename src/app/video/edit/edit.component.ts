ecaf3f67a3114e3576f24492fb8e227540432c7e
Vasu <vasumendapara93@gmail.com>
2024-03-01 10:29:47 +0530

    Updating the list of clips

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import IClip from 'src/app/models/clip.model';
 import { ClipService } from 'src/app/services/clip.service';
@@ -9,12 +9,13 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy, OnChanges{
-@Input() activeClip: IClip | null= null
-inSubmission = false
-showAlert = false
-alertColor = 'blue'
-alertMsg = 'Please wait! Updating Clip.'
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() activeClip: IClip | null = null
+  inSubmission = false
+  showAlert = false
+  alertColor = 'blue'
+  alertMsg = 'Please wait! Updating Clip.'
+  @Output() update = new EventEmitter()
 
   clipID = new FormControl('')
   title = new FormControl('', [
@@ -29,43 +30,51 @@ alertMsg = 'Please wait! Updating Clip.'
   constructor(
     private modal: ModalService,
     private clipService: ClipService
-    ) {  }
+  ) { }
 
-    ngOnInit(): void {
-        this.modal.register('editClip')
+  ngOnInit(): void {
+    this.modal.register('editClip')
+  }
+
+  ngOnDestroy() {
+    this.modal.unregister('editClip')
+  }
+
+  ngOnChanges() {
+    if (!this.activeClip) {
+      return
     }
+    this.inSubmission = false
+    this.showAlert = false
+    this.clipID.setValue(this.activeClip.docID!)
+    this.title.setValue(this.activeClip.title)
+  }
 
-    ngOnDestroy() {
-        this.modal.unregister('editClip')
+  async submit() {
+    if(!this.activeClip){
+      return
     }
+    this.inSubmission = true
+    this.showAlert = true
+    this.alertColor = 'blue'
+    this.alertMsg = 'Please wail! Updating Clip.'
 
-    ngOnChanges() {
-        if(!this.activeClip){
-          return
-        }
-        this.clipID.setValue(this.activeClip.docID!)
-        this.title.setValue(this.activeClip.title)
+    try {
+      await this.clipService.updateClip(this.clipID.value!, this.title.value!)
     }
-
-    async submit(){
-      this.inSubmission = true
-      this.showAlert = true
-      this.alertColor = 'blue'
-      this.alertMsg = 'Please wail! Updating Clip.'
-
-      try{
-      await this.clipService.updateClip(this.clipID.value!,this.title.value!)
-      }
-      catch(e){
-        this.inSubmission = false
-        this.alertColor = 'red'
-        this.alertMsg = 'Somethink went wrong! Please try again later.'
-        return
-      }
-
+    catch (e) {
       this.inSubmission = false
-      this.alertColor = 'green'
-      this.alertMsg = 'Success!'
-
+      this.alertColor = 'red'
+      this.alertMsg = 'Something went wrong! Please try again later.'
+      return
     }
+
+    this.activeClip.title = this.title.value
+    this.update.emit(this.activeClip)
+
+    this.inSubmission = false
+    this.alertColor = 'green'
+    this.alertMsg = 'Success!'
+
+  }
 }

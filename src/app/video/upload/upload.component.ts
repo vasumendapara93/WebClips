2d10c8429cc9a1ad26331901a118e099859d4d44
Vasu <vasumendapara93@gmail.com>
2024-03-08 18:02:13 +0530

    Initializing ffmpeg service

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,6 +7,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app'
 import { ClipService } from 'src/app/services/clip.service';
 import { Router } from '@angular/router';
+import { FfmpegService } from 'src/app/services/ffmpeg.service';
 
 @Component({
   selector: 'app-upload',
@@ -42,8 +43,10 @@ export class UploadComponent implements OnDestroy {
   constructor(private storage: AngularFireStorage,
     private auth: AngularFireAuth,
     private clipService: ClipService,
-    private router: Router) {
+    private router: Router,
+    public ffmpegService: FfmpegService) {
     auth.user.subscribe(user => this.user = user)
+    this.ffmpegService.init()
   }
 
   storeFile($event: Event) {

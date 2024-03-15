724c435aac688b619980c89b69b5c61dd29dbe2b
Vasu <vasumendapara93@gmail.com>
2024-03-15 12:13:57 +0530

    bypassing sanitization with pipes
    for screenshot

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -27,6 +27,7 @@ export class UploadComponent implements OnDestroy {
   showPercentage = false
   user: firebase.User | null = null
   task?: AngularFireUploadTask
+  screenshots: string[] = []
 
   title = new FormControl('', [
     Validators.required,
@@ -58,7 +59,7 @@ export class UploadComponent implements OnDestroy {
       return
     }
 
-    await this.ffmpegService.getScreenshots(this.file)
+    this.screenshots = await this.ffmpegService.getScreenshots(this.file)
 
     this.title.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
